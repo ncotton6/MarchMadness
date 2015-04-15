47184dcb4c18e87bb106c9df31f4ab1e273908ea
Nathaniel <ncotton6@gmail.com>
2015-04-15 10:01:45 -0400

    commenting

--- src/model/sim/NaiveBayesSim.java
+++ src/model/sim/NaiveBayesSim.java
@@ -13,8 +13,21 @@ import model.Tuple;
  */
 public class NaiveBayesSim implements GameSimulator {
 
+	/* (non-Javadoc)
+	 * @see model.GameSimulator#simulate(model.Game, int)
+	 */
 	@Override
 	public Tuple<Double, Double> simulate(Game game, int round) {
+		// what do winning teams have in common
+		
+		// what do losing teams have in common
+		
+		// classify team a as a loser or a winner
+		
+		// classify team b as a loser or a winner
+		
+		// which team is a bigger winner, or less of a loser
+		
 		return new Tuple<Double, Double>(1d, 0d);
 	}
 
