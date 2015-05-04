b4f7609a1da6b0c74fb98720e620742881016d32
Robin Li <rxl7906@rit.edu>
2015-05-04 16:00:41 -0400

    Decision Tree
    pre decision tree done

---
+++ R/GameSimulation.R
@@ -0,0 +1,440 @@
+seed_stats <- read.csv(file="seed_stats.csv")
+
+game_simulate <- function(teamA, teamB){
+  # assign point scale 
+  # +1 if one offensive/defensive attribute is greater than other
+  # -1 if turnovers/personal fouls is greater than other
+  team_a <- 0
+  team_b <- 0
+  if( (((teamA$Wins.Losses - teamB$Wins.Losses) > 0) || ((teamA$Wins.Losses - teamB$Wins.Losses) < 0)) && (abs(teamA$Wins.Losses - teamB$Wins.Losses) <= 0.20) ) {
+      if(teamA$Wins.Losses > teamB$Wins.Losses){
+        team_a = team_a + 1
+      } else {
+        team_b = team_b + 1
+      } 
+      print("Win Loss difference: ")
+      print(abs(teamA$Wins.Losses - teamB$Wins.Losses))
+      print("TEAM_A Score")
+      print(team_a)
+      print("TEAM_B Score")
+      print(team_b)
+      if( (((teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") > 0) || ((teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") < 0)) && (abs(teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") <= 0.04)){
+        if(teamA$"X2pts_Made.Attempt" > teamB$"X2pts_Made.Attempt"){
+          team_a = team_a + 1
+        } else if(teamA$"X2pts_Made.Attempt" < teamB$"X2pts_Made.Attempt"){
+          team_b = team_b + 1
+        }
+        print("2 pt made attempt difference")
+        print( abs(teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") )
+        print("TEAM_A Score")
+        print(team_a)
+        print("TEAM_B Score")
+        print(team_b)
+        
+        if( (((teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") > 0) || ((teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") < 0)) && (abs(teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") <= 0.04) ){
+          if(teamA$"X3pts_Made.Attempt" > teamB$"X3pts_Made.Attempt"){
+            team_a = team_a + 1
+          } else if(teamA$"X3pts_Made.Attempt" < teamB$"X3pts_Made.Attempt"){
+            team_b = team_b + 1
+          }
+          print("3 pt made attempt difference")
+          print( abs(teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") )
+          print("TEAM_A Score")
+          print(team_a)
+          print("TEAM_B Score")
+          print(team_b)
+          
+          if( (((teamA$Assists.Game - teamB$Assists.Game) > 0) || ((teamA$Assists.Game - teamB$Assists.Game) < 0)) && (abs(teamA$Assists.Game - teamB$Assists.Game) <= 4)) {
+            if(teamA$Assists.Game > teamB$Assists.Game){
+              team_a = team_a + 1
+            } else if(teamA$Assists.Game < teamB$Assists.Game){
+              team_b = team_b + 1
+            }
+            print("Assists difference: ")
+            print( abs(teamA$Assists.Game - teamB$Assists.Game) )
+            print("TEAM_A Score")
+            print(team_a)
+            print("TEAM_B Score")
+            print(team_b)
+            
+            if( (((teamA$Rebounds.Game - teamB$Rebounds.Game) > 0) || (abs(teamA$Rebounds.Game - teamB$Rebounds.Game) < 0)) && (abs(teamA$Rebounds.Game - teamB$Rebounds.Game) <= 4 )) {
+              if(teamA$Rebounds.Game > teamB$Rebounds.Game){
+                team_a = team_a + 1
+              } else if(teamA$Rebounds.Game < teamB$Rebounds.Game){
+                team_b = team_b + 1
+              }
+              print("Rebounds.Game")
+              print( abs(teamA$Rebounds.Game - teamB$Rebounds.Game) )
+              print("TEAM_A Score")
+              print(team_a)
+              print("TEAM_B Score")
+              print(team_b)
+              
+              if( (((teamA$Turnovers.Game - teamB$Turnovers.Game) > 0) || ((teamA$Turnovers.Game - teamB$Turnovers.Game) < 0)) && (abs(teamA$Turnovers.Game - teamB$Turnovers.Game) <= 4)) {
+                if(teamA$Turnovers.Game > teamB$Turnovers.Game){
+                  team_a = team_a - 1
+                } else if(teamA$Turnovers.Game < teamB$Turnovers.Game){
+                  team_b = team_b - 1
+                }
+                print("Turnovers")
+                print( abs(teamA$Turnovers.Game - teamB$Turnovers.Game) )
+                
+                print("TEAM_A Score")
+                print(team_a)
+                print("TEAM_B Score")
+                print(team_b)
+                if( (((teamA$Steals.Game - teamB$Steals.Game) > 0) || ((teamA$Steals.Game - teamB$Steals.Game) < 0)) && (abs(teamA$Steals.Game - teamB$Steals.Game) <= 2)){
+                  if(teamA$Steals.Game > teamB$Steals.Game){
+                    team_a = team_a + 1
+                  } else if(teamA$Steals.Game < teamB$Steals.Game){
+                    team_b = team_b + 1
+                  }
+                  print("Steals")
+                  print( abs(teamA$Steals.Game - teamB$Steals.Game) )
+                  print("TEAM_A Score")
+                  print(team_a)
+                  print("TEAM_B Score")
+                  print(team_b)
+                  
+                  if( (((teamA$Blocks.Game - teamB$Blocks.Game) > 0) || ((teamA$Blocks.Game - teamB$Blocks.Game) < 0)) && (abs(teamA$Blocks.Game - teamB$Blocks.Game) <= 2)){
+                    if(teamA$Blocks.Game > teamB$Blocks.Game){
+                      team_a = team_a + 1
+                    } else if(teamA$Blocks.Game < teamB$Blocks.Game){
+                      team_b = team_b + 1
+                    }
+                    print("Blocks")
+                    print( abs(teamA$Blocks.Game - teamB$Blocks.Game) )
+                    
+                    print("TEAM_A Score")
+                    print(team_a)
+                    print("TEAM_B Score")
+                    print(team_b)
+                    # compare team_a team_b scores
+                    if(team_a > team_b){
+                      return(teamA)
+                    } else{
+                      return(teamB)
+                    }
+                  } else if( (abs(teamA$Blocks.Game - teamB$Blocks.Game) > 2) && ((teamA$Blocks.Game - teamB$Blocks.Game) > 0)){
+                    print(teamA)
+                    return(teamA)
+                  } else {
+                    print(teamB)
+                    return(teamB)
+                  }
+                } else if( (abs(teamA$Steals.Game - teamB$Steals.Game) > 2) && ((teamA$Steals.Game - teamB$Steals.Game) > 0)){
+                  print(teamA)
+                  return(teamA)
+                } else {
+                  print(teamB)
+                  return(teamB)
+                }
+              } else if( (abs(teamA$Turnovers.Game - teamB$Turnovers.Game) > 4) && ((teamA$Turnovers.Game - teamB$Turnovers.Game) > 0)){
+                print(teamB)
+                return(teamB)
+              } else {
+                print(teamA)
+                return(teamA)
+              }
+            } else if( (abs(teamA$Rebounds.Game - teamB$Rebounds.Game) > 4 ) && ((teamA$Rebounds.Game - teamB$Rebounds.Game) > 0) ){
+              print(teamA)
+              return(teamA)
+            } else{
+              print(teamB)
+              return(teamB)
+            } 
+          } else if( (abs(teamA$Assists.Game - teamB$Assists.Game) > 4) && ((teamA$Assists.Game - teamB$Assists.Game) > 0)){
+            print(teamA)
+            return(teamA)
+          } else{
+            print(teamB)
+            return(teamB)
+          }
+        } else if( (abs(teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") > 0.04) && ((teamA$"X3pts_Made.Attempt" - teamB$"X3pts_Made.Attempt") > 0) ){
+          print(teamA)
+          return(teamA)
+        } else{
+          print(teamB)
+          return(teamB)
+        }
+      } else if( (abs(teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") > 0.03) && ((teamA$"X2pts_Made.Attempt" - teamB$"X2pts_Made.Attempt") > 0) ){
+        print(teamA)
+        return(teamA)
+      } else{
+        print(teamB)
+        return(teamB)
+      }
+  } else if( (abs(teamA$Wins.Losses - teamB$Wins.Losses) > 0.20) && ((teamA$Wins.Losses - teamB$Wins.Losses) > 0) ){
+    print(teamA)
+    return(teamA)
+  } else {
+    print(teamB)
+    return(teamB)
+  }
+}
+
+# separate by regions
+# East = W
+# Midwest = X
+# South = Y
+# West = Z
+East <- data.frame(seed_stats[0,])
+Midwest <- data.frame(seed_stats[0,])
+South <- data.frame(seed_stats[0,])
+West <- data.frame(seed_stats[0,])
+
+East_excp <- data.frame(seed_stats[0,])
+Midwest_excp <- data.frame(seed_stats[0,])
+South_excp <- data.frame(seed_stats[0,])
+West_excp <- data.frame(seed_stats[0,])
+
+Selection_Sunday_winners <- data.frame(seed_stats[0,])
+
+for(i in 1: length(seed_stats$seed)){
+  if(grepl("W", seed_stats$seed[i])){
+    print("IN THE EAST")
+    #print(seed_stats[i,])
+    if(grepl("a", seed_stats$seed[i])){
+      East_excp <- rbind(East_excp, seed_stats[i,])
+    } else if(grepl("b", seed_stats$seed[i])){
+      East_excp <- rbind(East_excp, seed_stats[i,])
+      #after the b was added play a and b
+      winner <- game_simulate(East_excp[1,] , East_excp[2,])
+      print("Winner:")
+      print(winner)
+      #add the winner of them
+      East <- rbind(East, winner)
+      Selection_Sunday_winners <- rbind(Selection_Sunday_winners, winner)
+    } else {
+      East <- rbind(East, seed_stats[i,])
+    }
+  }
+  if(grepl("X", seed_stats$seed[i])){
+    print("IN THE MIDWEST")
+    #print(seed_stats[i,])
+    if(grepl("a", seed_stats$seed[i])){
+      Midwest_excp <- rbind(Midwest_excp, seed_stats[i,])
+    } else if(grepl("b", seed_stats$seed[i])){
+      Midwest_excp <- rbind(Midwest_excp, seed_stats[i,])
+      #after the b was added play a and b
+      winner <- game_simulate(Midwest_excp[1,] , Midwest_excp[2,])
+      print("Winner:")
+      print(winner)
+      #add the winner of them
+      Midwest <- rbind(Midwest, winner)
+      Selection_Sunday_winners <- rbind(Selection_Sunday_winners, winner)
+    } else {
+      Midwest <- rbind(Midwest, seed_stats[i,])
+    }
+  }
+  if(grepl("Y", seed_stats$seed[i])){
+    print("IN THE SOUTH")
+    #print(seed_stats[i,])
+    #South <- rbind(South, seed_stats[i,])
+    if(grepl("a", seed_stats$seed[i])){
+      South_excp <- rbind(South_excp, seed_stats[i,])
+    } else if(grepl("b", seed_stats$seed[i])){
+      South_excp <- rbind(South_excp, seed_stats[i,])
+      #after the b was added play a and b
+      winner <- game_simulate(South_excp[1,] , South_excp[2,])
+      print("Winner:")
+      print(winner)
+      #add the winner of them
+      South <- rbind(South, winner)
+      Selection_Sunday_winners <- rbind(Selection_Sunday_winners, winner)
+    } else {
+      South <- rbind(South, seed_stats[i,])
+    }
+  }
+  if(grepl("Z", seed_stats$seed[i])){
+    print("IN THE WEST")
+    #West <- rbind(West, seed_stats[i,])
+    if(grepl("a", seed_stats$seed[i])){
+      West_excp <- rbind(West_excp, seed_stats[i,])
+    } else if(grepl("b", seed_stats$seed[i])){
+      West_excp <- rbind(West_excp, seed_stats[i,])
+      #after the b was added play a and b
+      winner <- game_simulate(West_excp[1,] , West_excp[2,])
+      print("Winner:")
+      print(winner)
+      #add the winner of them
+      West <- rbind(West, winner)
+      Selection_Sunday_winners <- rbind(Selection_Sunday_winners, winner)
+    } else {
+      West <- rbind(West, seed_stats[i,])
+    }
+  }
+}
+
+East_round2_winners <- data.frame(seed_stats[0,])
+Midwest_round2_winners <- data.frame(seed_stats[0,])
+South_round2_winners <- data.frame(seed_stats[0,])
+West_round2_winners <- data.frame(seed_stats[0,])
+
+# play the 2nd round
+start <- 1
+end <- 16
+length <- 8
+for(i in 1: length){
+  # play two East teams
+  winner <- game_simulate(East[start,], East[end,])
+  print("East Round 2 Winner: ")
+  print(winner)
+  East_round2_winners <- rbind(East_round2_winners, winner)
+  
+  # play two Midwest teams
+  winner <- game_simulate(Midwest[start,], Midwest[end,])
+  print("Midwest Round 2 Winner: ")
+  print(winner)
+  Midwest_round2_winners <- rbind(Midwest_round2_winners, winner)
+  
+  # play two South teams
+  winner <- game_simulate(South[start,], South[end,])
+  print("South Round 2 Winner: ")
+  print(winner)
+  South_round2_winners <- rbind(South_round2_winners, winner)
+  
+  # play two West teams
+  winner <- game_simulate(West[start,], West[end,])
+  print("West Round 2 Winner: ")
+  print(winner)
+  West_round2_winners <- rbind(West_round2_winners, winner)
+  
+  start = start + 1
+  end = end - 1
+}
+
+East_round3_winners <- data.frame(seed_stats[0,])
+Midwest_round3_winners <- data.frame(seed_stats[0,])
+South_round3_winners <- data.frame(seed_stats[0,])
+West_round3_winners <- data.frame(seed_stats[0,])
+
+# play the 3rd round
+start <- 1
+end <- 8
+length <- 4
+for(i in 1: length) {
+  # play two teams
+  winner <- game_simulate(East_round2_winners[start,], East_round2_winners[end,])
+  print("East Round 3 Winner: ")
+  print(winner)
+  East_round3_winners <- rbind(East_round3_winners, winner)
+  
+  # play two Midwest teams
+  winner <- game_simulate(Midwest_round2_winners[start,], Midwest_round2_winners[end,])
+  print("Midwest Round 3 Winner: ")
+  print(winner)
+  Midwest_round3_winners <- rbind(Midwest_round3_winners, winner)
+  
+  # play two South teams
+  winner <- game_simulate(South_round2_winners[start,], South_round2_winners[end,])
+  print("South Round 3 Winner: ")
+  print(winner)
+  South_round3_winners <- rbind(South_round3_winners, winner)
+  
+  # play two West teams
+  winner <- game_simulate(West_round2_winners[start,], West_round2_winners[end,])
+  print("West Round 3 Winner: ")
+  print(winner)
+  West_round3_winners <- rbind(West_round3_winners, winner)
+  
+  start = start + 1
+  end = end - 1
+}
+
+East_round4_winners <- data.frame(seed_stats[0,])
+Midwest_round4_winners <- data.frame(seed_stats[0,])
+South_round4_winners <- data.frame(seed_stats[0,])
+West_round4_winners <- data.frame(seed_stats[0,])
+
+# play the 4th round
+start <- 1
+end <- 4
+length <- 2
+for(i in 1: length) {
+  # play two teams
+  winner <- game_simulate(East_round3_winners[start,], East_round3_winners[end,])
+  print("East Round 3 Winner: ")
+  print(winner)
+  East_round4_winners <- rbind(East_round4_winners, winner)
+  
+  # play two Midwest teams
+  winner <- game_simulate(Midwest_round3_winners[start,], Midwest_round3_winners[end,])
+  print("Midwest Round 3 Winner: ")
+  print(winner)
+  Midwest_round4_winners <- rbind(Midwest_round4_winners, winner)
+  
+  # play two South teams
+  winner <- game_simulate(South_round3_winners[start,], South_round3_winners[end,])
+  print("South Round 3 Winner: ")
+  print(winner)
+  South_round4_winners <- rbind(South_round4_winners, winner)
+  
+  # play two West teams
+  winner <- game_simulate(West_round3_winners[start,], West_round3_winners[end,])
+  print("West Round 3 Winner: ")
+  print(winner)
+  West_round4_winners <- rbind(West_round4_winners, winner)
+  
+  start = start + 1
+  end = end - 1
+}
+
+East_round5_winners <- data.frame(seed_stats[0,])
+Midwest_round5_winners <- data.frame(seed_stats[0,])
+South_round5_winners <- data.frame(seed_stats[0,])
+West_round5_winners <- data.frame(seed_stats[0,])
+FinalFour <- data.frame(seed_stats[0,])
+
+# play the 5th round
+start <- 1
+end <- 2
+length <- 1
+for(i in 1: length) {
+  # play two teams
+  winner <- game_simulate(East_round4_winners[start,], East_round4_winners[end,])
+  print("East Round 3 Winner: ")
+  print(winner)
+  East_round5_winners <- rbind(East_round5_winners, winner)
+  FinalFour <- rbind(FinalFour, winner)
+  
+  # play two Midwest teams
+  winner <- game_simulate(Midwest_round4_winners[start,], Midwest_round4_winners[end,])
+  print("Midwest Round 3 Winner: ")
+  print(winner)
+  Midwest_round5_winners <- rbind(Midwest_round5_winners, winner)
+  FinalFour <- rbind(FinalFour, winner)
+  
+  # play two South teams
+  winner <- game_simulate(South_round4_winners[start,], South_round4_winners[end,])
+  print("South Round 3 Winner: ")
+  print(winner)
+  South_round5_winners <- rbind(South_round5_winners, winner)
+  FinalFour <- rbind(FinalFour, winner)
+  
+  # play two West teams
+  winner <- game_simulate(West_round4_winners[start,], West_round4_winners[end,])
+  print("West Round 3 Winner: ")
+  print(winner)
+  West_round5_winners <- rbind(West_round5_winners, winner)
+  FinalFour <- rbind(FinalFour, winner)
+  
+  start = start + 1
+  end = end - 1
+}
+
+Championship <- data.frame(seed_stats[0,])
+
+# play final four
+# play the first two teams in final four
+winner <- game_simulate(FinalFour[1,], FinalFour[2,])
+Championship <- rbind(Championship, winner)
+
+# play the last two teams in final four
+winner <- game_simulate(FinalFour[3,], FinalFour[4,])
+Championship <- rbind(Championship, winner)
+
+Champion <- data.frame(seed_stats[0,])
+# play the championship game
+winner <- game_simulate(Championship[1,], Championship[2,])
+Champion <- rbind(Champion, winner)
--- R/regular_season_detailed_results_2015.r
+++ R/regular_season_detailed_results_2015.r
@@ -1,10 +1,10 @@
-# read season stats and teams
+# read season stats, teams, and tournament seeding
 gamestats <- read.csv(file="regular_season_detailed_results_2015.csv")
 teams <- read.csv(file="teams.csv")
 tournament <- read.csv(file="tourney_seeds_2015.csv")
 
-
-# add stats to the teams table
+# initialize teams
+teams["No.Games"] <- NA
 teams["wins"] <- NA
 teams["losses"] <- NA
 teams["Wins/Losses"] <- NA
@@ -12,6 +12,7 @@ teams["2pts_Made/Attempt"] <- NA
 teams["3pts_Made/Attempt"] <- NA
 teams["FT_Made/Attempt"] <- NA
 teams["Rebounds"] <- NA
+teams["Rebounds/Game"] <- NA
 teams["Assists"] <- NA
 teams["Turnovers"] <- NA
 teams["Steals"] <- NA
@@ -24,85 +25,122 @@ for(i in 1:length(teams$team_id)){
   # get the numbers for each game stats
   team_wins = 0
   team_losses = 0
-  wfgm = 0
-  wfga = 0
-  wfgm3 = 0
-  wfga3 = 0
-  wftm = 0
-  wfta = 0
-  rebounds = 0
-  wast = 0
-  wto = 0
-  wstl = 0
-  wblk = 0
-  wpf = 0
-  # with one team loop to find the wins
+  tfgm = 0
+  tfga = 0
+  tfgm3 = 0
+  tfga3 = 0
+  tftm = 0
+  tfta = 0
+  trebounds = 0
+  tast = 0
+  tto = 0
+  tstl = 0
+  tblk = 0
+  tpf = 0
+  # loop to find when team was a winner
   for(j in 1:length(gamestats$wteam)){
     # count how many times you see this team wins
     if(teams$team_id[i] == gamestats$wteam[j]){
       team_wins = team_wins + 1
-      wfgm = wfgm + gamestats$wfgm[j]
-      wfga = wfga + gamestats$wfga[j]
-      wfgm3 = wfgm3 + gamestats$wfgm3[j]
-      wfga3 = wfga3 + gamestats$wfga3[j]
-      wftm = wftm + gamestats$wftm[j]
-      wfta = wfta + gamestats$wfta[j]
-      rebounds = rebounds + gamestats$wor[j] + gamestats$wdr[j]
-      wast = wast + gamestats$wast[j]
-      wto = wto + gamestats$wto[j]
-      wstl = wstl + gamestats$wstl[j]
-      wblk = wblk + gamestats$wblk[j]
-      wpf = wpf + gamestats$wpf[j]
+      tfgm = tfgm + gamestats$wfgm[j]
+      tfga = tfga + gamestats$wfga[j]
+      tfgm3 = tfgm3 + gamestats$wfgm3[j]
+      tfga3 = tfga3 + gamestats$wfga3[j]
+      tftm = tftm + gamestats$wftm[j]
+      tfta = tfta + gamestats$wfta[j]
+      trebounds = trebounds + gamestats$wor[j] + gamestats$wdr[j]
+      tast = tast + gamestats$wast[j]
+      tto = tto + gamestats$wto[j]
+      tstl = tstl + gamestats$wstl[j]
+      tblk = tblk + gamestats$wblk[j]
+      tpf = tpf + gamestats$wpf[j]
     }
+  }
+  # loop to see when team was a loser
+  for(j in 1:length(gamestats$lteam)){
     # with one team loop to find losses
     if(teams$team_id[i] == gamestats$lteam[j]){
       team_losses = team_losses + 1
-      wfgm = wfgm + gamestats$wfgm[j]
-      wfga = wfga + gamestats$wfga[j]
-      wfgm3 = wfgm3 + gamestats$wfgm3[j]
-      wfga3 = wfga3 + gamestats$wfga3[j]
-      wftm = wftm + gamestats$wftm[j]
-      wfta = wfta + gamestats$wfta[j]
-      rebounds = gamestats$wor[j] + gamestats$wdr[j]
-      wast = wast + gamestats$wast[j]
-      wto = wto + gamestats$wto[j]
-      wstl = wstl + gamestats$wstl[j]
-      wblk = wblk + gamestats$wblk[j]
-      wpf = wpf + gamestats$wpf[j]
+      tfgm = tfgm + gamestats$lfgm[j]
+      tfga = tfga + gamestats$lfga[j]
+      tfgm3 = tfgm3 + gamestats$lfgm3[j]
+      tfga3 = tfga3 + gamestats$lfga3[j]
+      tftm = tftm + gamestats$lftm[j]
+      tfta = tfta + gamestats$lfta[j]
+      trebounds = trebounds + gamestats$lor[j] + gamestats$ldr[j]
+      twast = tast + gamestats$last[j]
+      twto = tto + gamestats$lto[j]
+      tstl = tstl + gamestats$lstl[j]
+      tblk = tblk + gamestats$lblk[j]
+      tpf = tpf + gamestats$lpf[j]
     }
-    #add to teams table wins column
-    w_l_ratio <- team_wins / (team_wins + team_losses)
-    teams$wins[i] <- team_wins
-    teams$losses[i] <- team_losses
-    teams$"Wins/Losses"[i] <- w_l_ratio
-    
-    #number of games
-    number_games = (team_wins + team_losses)
-    
-    # get ratios
-    wfgm_wfga_ratio = wfgm / wfga
-    wfgm3_wfga3_ratio = wfgm3 / wfga3
-    wftm_wfta_ratio = wftm / wfta
-    
-    teams$"2pts_Made/Attempt"[i] = wfgm_wfga_ratio
-    teams$"3pts_Made/Attempt"[i] = wfgm3_wfga3_ratio
-    teams$"FT_Made/Attempt"[i] = wftm_wfta_ratio
-    teams$Rebounds[i] = rebounds
-    teams$Assists[i] = wast
-    teams$Turnovers[i] = wto
-    teams$Steals[i] = wstl
-    teams$Blocks[i] = wblk
-    teams$PersonalFoul[i] = wpf
   }
+    
+  teams$"No.Games"[i] <- team_wins + team_losses
+  #add to teams table wins column
+  w_l_ratio <- team_wins / (team_wins + team_losses)
+  teams$wins[i] <- team_wins
+  teams$losses[i] <- team_losses
+  teams$"Wins/Losses"[i] <- w_l_ratio
+  
+  #number of games
+  number_games = (team_wins + team_losses)
+  
+  # get ratios
+  tfgm_tfga_ratio = tfgm / tfga
+  tfgm3_tfga3_ratio = tfgm3 / tfga3
+  tftm_tfta_ratio = tftm / tfta
+  
+  teams$"2pts_Made/Attempt"[i] = tfgm_tfga_ratio
+  teams$"3pts_Made/Attempt"[i] = tfgm3_tfga3_ratio
+  teams$"FT_Made/Attempt"[i] = tftm_tfta_ratio
+  teams$Rebounds[i] = trebounds
+  teams$"Rebounds/Game"[i] = trebounds/number_games
+  teams$Assists[i] = tast/number_games
+  teams$Turnovers[i] = tto/number_games
+  teams$Steals[i] = tstl/number_games
+  teams$Blocks[i] = tblk/number_games
+  teams$PersonalFoul[i] = tpf/number_games
 }
 
 #tournament
-for(1 in k:length(tournament$team)){
- #tournament$team[k]
- for(1 in m:length(teams$team_id)){
+tournament["name"] <- NA
+tournament["wins"] <- NA
+tournament["losses"] <- NA
+tournament["Wins/Losses"] <- NA
+tournament["2pts_Made/Attempt"] <- NA
+tournament["3pts_Made/Attempt"] <- NA
+tournament["FT_Made/Attempt"] <- NA
+#tournament["Rebounds"] <- NA
+tournament["Rebounds/Game"] <- NA
+tournament["Assists/Game"] <- NA
+tournament["Turnovers/Game"] <- NA
+tournament["Steals/Game"] <- NA
+tournament["Blocks/Game"] <- NA
+tournament["PersonalFoul/Game"] <- NA
+
+for(k in 1:length(tournament$team)){
+ for(m in 1:length(teams$team_id)){
    if(tournament$team[k] == teams$team_id[m]){
-     
+     tournament$name[k] = as.character(teams$team_name[m])
+     tournament$wins[k] = teams$wins[m]
+     tournament$losses[k] = teams$losses[m]
+     tournament$"Wins/Losses"[k] = teams$"Wins/Losses"[m]
+     tournament$"2pts_Made/Attempt"[k] = teams$"2pts_Made/Attempt"[m]
+     tournament$"3pts_Made/Attempt"[k] = teams$"3pts_Made/Attempt"[m]
+     tournament$"FT_Made/Attempt"[k] = teams$"FT_Made/Attempt"[m]
+     #tournament$Rebounds[k] = teams$Rebounds[m]
+     tournament$"Rebounds/Game"[k] = teams$"Rebounds/Game"[m]
+     tournament$"Assists/Game"[k] = teams$Assists[m]
+     tournament$"Turnovers/Game"[k] = teams$Turnovers[m]
+     tournament$"Steals/Game"[k] = teams$Steals[m]
+     tournament$"Blocks/Game"[k] = teams$Blocks[m]
+     tournament$"FT_Made/Attempt"[k] = teams$"FT_Made/Attempt"[m]
+     tournament$"PersonalFoul/Game"[k] = teams$PersonalFoul[m]
    }
  }
 }
 
+
+# write out to csv file
+seed_stats <- write.csv(tournament, file="seed_stats.csv")
