5c7f70acfe59969b164af26704ae860fad560e31
Nathaniel <ncotton6@gmail.com>
2015-05-05 07:52:26 -0400

    refactored/created knn

--- src/model/Prototype.java
+++ src/model/Prototype.java
@@ -1,4 +1,4 @@
-package model.sim;
+package model;
 
 import java.util.ArrayList;
 
---
+++ src/model/sim/KNNSim.java
@@ -0,0 +1,15 @@
+package model.sim;
+
+import model.Game;
+import model.GameSimulator;
+import model.Tuple;
+
+public class KNNSim implements GameSimulator {
+
+	@Override
+	public Tuple<Double, Double> simulate(Game game, int round) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
